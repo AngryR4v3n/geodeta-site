fc6b132d77e98a9253b25df26578966f9fb38653
Francisco Molina <molinajimenez01@gmail.com>
2023-08-10 00:27:22 +0200

    Desktop nav finished, added bootstrap carousel

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.set('view engine', 'html');
 app.get('/', (req, res) => {
   let siteMap = [
     { "title": "Link 1", "hasDropdown": false, "link": "/link1" },
-    { "title": "Link 2", "hasDropdown": true, "link": "/link2", "sublink": [{ "title": "A", "link": "/link2/a" }] }
+    { "title": "Services", "hasDropdown": true, "link": "/link2", "sublink": [
+      { "title": "Experience", "link": "/link2/a", "description": "Over 200 projects, across Poland" },
+      { "title": "Get a quota", "link": "/link2/a", "description": "Accessible, and financiable quotas." }
+    ] },
+    { "title": "Gallery", "hasDropdown": true, "link": "/link3", "sublink": [{ "title": "Photos", "link": "/link3/b", "description": "Check our latest projects!" }] }
   ];
   res.render('body', { navBarOptions: siteMap, title: "Hello" });
 })
