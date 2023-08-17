eba48696f8f98230e1029badea60e5ee84264af3
Francisco Molina <molinajimenez01@gmail.com>
2023-08-18 00:06:51 +0200

    Completed popup, fixed js loading. Started carousel with auto generated slides and formatted slides

--- index.js
+++ index.js
@@ -11,16 +11,33 @@ app.use("/styles", express.static(path.join(__dirname, "styles")));
 app.set('view engine', 'html');
 
 
-app.get('/', (req, res) => {
-  let siteMap = [
-    { "title": "Link 1", "hasDropdown": false, "link": "/link1" },
-    { "title": "Services", "hasDropdown": true, "link": "/link2", "sublink": [
-      { "title": "Experience", "link": "/link2/a", "description": "Over 200 projects, across Poland" },
-      { "title": "Get a quota", "link": "/link2/a", "description": "Accessible, and financiable quotas." }
-    ] },
-    { "title": "Gallery", "hasDropdown": true, "link": "/link3", "sublink": [{ "title": "Photos", "link": "/link3/b", "description": "Check our latest projects!" }] }
-  ];
-  res.render('body', { navBarOptions: siteMap, title: "Hello" });
+//Allow all https src
+app.use((req, res, next)=> {
+  res.setHeader('Content-Security-Policy', "script-src 'self' https: unsafe-inline;");
+  next();
 })
+
+let siteMap = [
+  { "title": "Link 1", "hasDropdown": false, "link": "/link1" },
+  { "title": "Services", "hasDropdown": true, "link": "/link2", "sublink": [
+    { "title": "Experience", "link": "/link2/a", "description": "Over 200 projects, across Poland" },
+    { "title": "Get a quota", "link": "/link2/a", "description": "Accessible, and financiable quotas." }
+  ] },
+  { "title": "Gallery", "hasDropdown": true, "link": "/link3", "sublink": [{ "title": "Photos", "link": "/link3/b", "description": "Check our latest projects!" }] }
+];
+let carouselSlides = [
+  {"photoPath": "/public/chicago.jpg", "photoDesc": "a nice photo 1", "description": "This is a test 1", "id": 0},
+  {"photoPath": "/public/la.jpg" , "photoDesc": "a nice photo 2", "description": "This is a test 2", "id": 1},
+  {"photoPath": "/public/ny.jpg" , "photoDesc": "a nice photo 3", "description": "This is a test 3", "id": 2},
+];
+app.get('/', (req, res) => {  
+  res.render('landing.html', {carouselSlides: carouselSlides, navBarOptions: siteMap, title: "Hello"});
+});
+
+app.get('/test', (req, res) => {
+
+  res.render('testing.html', { navBarOptions: siteMap, title: "Hello" })
+
+});
 
 app.listen(port, null)
