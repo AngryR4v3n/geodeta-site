9cf53269046935eeebfab7ceae8add52a4e495ca
Francisco Molina <molinajimenez01@gmail.com>
2024-01-05 14:08:55 +0100

    updated descriptions

--- siteInfo/siteInfo.js
+++ siteInfo/siteInfo.js
@@ -3,7 +3,7 @@ module.exports = {
         "en": {
             "main": "Land Surveyor Anna Dakowicz",
             "services": "Our services",
-            "pricing": "Pricing",
+            "pricing": "Price list",
             "gallery": "Gallery",
             "aboutus": "About us",
             "contactus": "Contact us"
@@ -44,45 +44,45 @@ module.exports = {
     },
     carouselSlides: {
         "en": [
-            { "photoPath": "/public/male_pic.png", "photoDesc": "a nice photo 1", "description": "Full professionalism. Order completed on time. I strongly recommend.", "id": 0, "author": "Czesław P.", "enterprise": "" },
-            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 2", "description": "A lot of experience and great contact with the customer made it possible to quickly implement the service. Ms. Anna uses modern equipment at work so all measurements are very accurate and there were no corrections. I will definitely also cooperate with Ms. Anna in the future.", "id": 1, "author": "Dorota W.", "enterprise": "" },
-            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 3", "description": "If someone is looking for a really great surveyor,I highly recommend.  Ms. Anna is one of the best and most accurate surveyors in their profession, has worked in this profession for many years. I recommend to everyone with a clear conscience.", "id": 2, "author": "Aleksandra B.", "enterprise": "" },
+            { "photoPath": "/public/male_pic.png", "photoDesc": "a nice photo 1", "description": "Full professionalism. The service was done on time. I definitely recommend. ", "id": 0, "author": "Czesław P.", "enterprise": "" },
+            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 2", "description": "Extensive experience and great customer contact.  No corrections were needed.", "id": 1, "author": "Dorota W.", "enterprise": "" },
+            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 3", "description": "Fast implementation, a well-done service.", "id": 2, "author": "Aleksandra B.", "enterprise": "" },
         ],
         "pl": [
-            { "photoPath": "/public/male_pic.png", "photoDesc": "a nice photo 1", "description": "Pełen profesjonalizm. Zlecenie wykonane w terminie. Zdecydowanie polecam. ", "id": 0, "author": "Czesław P.", "enterprise": "" },
-            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 2", "description": "Duże doświadczenie i świetny kontakt z klientem umożliwił szybką realizację usługi. Pani Ania wykorzystuje nowoczesny sprzęt w pracy więc wszystkie pomiary są bardzo dokładne i obyło się bez poprawek. Na pewno w przyszłości również podejmę współpracę z Panią Anią.", "id": 1, "author": "Dorota W.", "enterprise": "" },
-            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 3", "description": "Jeśli ktoś ze szuka naprawdę świetnego geodety,to polecam serdecznie.  Pani Ania jest jednym z najlepszych i najdokładniejszych geodetów w swoim fachu,przepracowała w tym zawodzie już wiele lat. Polecam każdemu z czystym sumieniem.", "id": 2, "author": "Aleksandra B.", "enterprise": "" },
+            { "photoPath": "/public/male_pic.png", "photoDesc": "a nice photo 1", "description": "Pełen profesjonalizm. Zlecenie wykonane w terminie. Zdecydowanie polecam.", "id": 0, "author": "Czesław P.", "enterprise": "" },
+            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 2", "description": "Duże doświadczenie i świetny kontakt z klientem. Obyło się bez poprawek.", "id": 1, "author": "Dorota W.", "enterprise": "" },
+            { "photoPath": "/public/female_pic.png", "photoDesc": "a nice photo 3", "description": "Szybki czas realizacji, dobrze wykonana usługa.", "id": 2, "author": "Aleksandra B.", "enterprise": "" },
         ]
     },
     landing: {
         "en": {
             "landingIntro": {
                 "MainTitle": "Ania Dakowicz",
-                "MainTitleUnderlined": "the land surveyor",
-                "MainTitleDesc": "Ania is a professional geodeta in the Podlaskie area of Poland, years of experience and complete satisfaction. Ania is the person your project.",
+                "MainTitleUnderlined": "land surveyor",
+                "MainTitleDesc": "I provide professional surveying services in the Podlaskie voivodeship.",
                 "YellowButtonAboutUs": "Get to know Ania",
                 "BlueButtonProjects": "Projects"
             },
-            "secondTitle": "We make your projects achievable",
-            "thirdTitle": "Through hard work and expertise",
-            "tool1": { "title": "Precision tools", "description": "We revolutionize your land surveying projects with our precision tools. Achieve measurements like never before for accurate data that drives better decisions." },
-            "tool2": { "title": "Modern software", "description": "We utilize AutoCAD. Our expertise in this industry-standard software ensures precise and professional mapping." },
-            "tool3": { "title": "Experience", "description": "Our expertise of a dedicated land surveyor. With a proven track record of precision and reliability in land surveying." },
+            "secondTitle": "Do your project with our support.",
+            "thirdTitle": "Through hard work and knowledge",
+            "tool1": { "title": "Precision tools", "description": "Modern tools make it possible to obtain accurate data for the implementation of surveying projects." },
+            "tool2": { "title": "Modern software", "description": "EwMapa, TurboMap, GstarCAD, GoKart, Mikromap, WinKalk, C-GEO and other programs allow you to prepare digital and analogue documentation." },
+            "tool3": { "title": "Experience", "description": "Over thirteen years of experience in land surveying." },
             "solutionsButton": "View Solutions",
-            "lastMessage": "We are glad you are considering us.\n Looking forward to work with you.",
+            "lastMessage": "We are glad that you are considering working with us",
             "checkers": {
                 "titleRight": "Care, Experience, Precision.",
-                "listItemRight1Keyword": "Service focused.",
-                "listItemRight1": "Our customer friendly approach is dedicated to providing excellent support and making sure you're pleased with our service.",
+                "listItemRight1Keyword": "Focus on services.",
+                "listItemRight1": " We provide an individual approach to each project, which ensures the satisfaction of our customers.",
                 "listItemRight2Keyword": "Years of experience.",
-                "listItemRight2": "Experience is our foundation, with decades of proven expertise in land surveying.",
-                "listItemRight3Keyword": "Time focused.",
-                "listItemRight3": "We strive for productivity as a top priority, consistently delivering efficient surveying solutions tailored to your project's demands.",
-                "titleLeft": "Projects we have worked with",
+                "listItemRight2": " Experience is the foundation of our company. Many years of activity in the industry and numerous professional trainings make every project possible.",
+                "listItemRight3Keyword": "Fast implementation time.",
+                "listItemRight3": " Our priority is productivity and fast delivery of efficient solutions customized to project requirements.",
+                "titleLeft": "Types of project we have implemented",
                 "listItemLeft1Keyword": "Railways.",
-                "listItemLeft1": " We have worked with Polish Railways helping to improve our country's infrastructure.",
+                "listItemLeft1": " We have worked with the Polish State Railways, helping to improve the infrastructure of our country.",
                 "listItemLeft2Keyword": "Suburbs.",
-                "listItemLeft2": " Our experience in the housing sector helps us to maximize the usage of the available land.",
+                "listItemLeft2": " Our experience in the residential sector helps us to maximise the use of available land.",
                 "listItemLeft3Keyword": "Apartments.",
                 "listItemLeft3": " Our housing sector also expands to bigger projects, which require a broad list of abilities.",
 
@@ -163,16 +163,16 @@ module.exports = {
             "titleItem7": "C-GEO",
             "secondTitle": "Programs we are working with",
             "viewMoreText": "Learn more",
-            "cardTitle1": "Division of land and real estate",
-            "cardText1": "The geodetic division of a plot of land and real estate is the physical separation of two or more plots of land from the original registered plot, containing the boundaries of the plot before the division.",
-            "cardTitle2": "Building staking",
-            "cardText2": "Geodetic delineation of buildings in the field serves the spatial location of these objects in accordance with the construction project.",
+            "cardTitle1": "Division of land and property",
+            "cardText1": "Surveying subdivision of a parcel and property is the physical separation of two or more parcels from the original cadastral parcel, contained within the boundaries of the parcel before the subdivision. The result of this subdivision is a new configuration of parcels with a new layout of boundary lines and areas. The documentation created during the division is in the form of a technical report. Detailed recommendations on the cadastral subdivision of the parcel are included in the local land use plan. The cost of subdivision of a parcel depends on its size.",
+            "cardTitle2": "Set out of buildings",
+            "cardText2": "The surveying set out of buildings is used to place these new objects in space accordingly to the construction project. It is important to maintain appropriate distances between the newly designated objects and the already existing objects within relation to the boundary lines of the property. The surveying set out of buildings is one of the preparatory works that can only be done in the area covered by the building permit or notification.",
             "cardTitle3": "Delineation of property boundaries",
             "cardText3": "Boundary marks indicate the location of the boundary line, while boundary points, mark where the property ends.",
             "cardTitle4": "Land surveying for construction projects",
             "cardText4": "Surveying services for construction projects are all activities performed during the implementation of any construction project.",
-            "cardTitle5": "Land surveying on railroad areas",
-            "cardText5": "Surveying work on railroad areas involves precise measurements and assessments of tracks and infrastructure to maintain safety and operational efficiency, ensuring the integrity of the rail network.",
+            "cardTitle5": "Surveying works in railway areas",
+            "cardText5": "Surveying work in railway areas involves precise measurements and assessments of tracks as well as infrastructure to maintain safety and operational efficiency, ensuring the integrity of the rail network.",
             "cardTitle6": "Maps for project purposes",
             "cardText6": "Maps for land surveying purposes are indispensable tools that visually depict surveyed land parcels, boundaries, and topographical features",
         },
@@ -189,15 +189,15 @@ module.exports = {
             "desc": "Dzięki bogatemu doświadczeniu jako wykonawca specjalizujący się w geodezji, staliśmy się wykwalifikowanymi profesjonalistami w zakresie dokładnych pomiarów i mapowania gruntów dla różnych projektów budowlanych i deweloperskich. Nasze doświadczenie w wykorzystywaniu zaawansowanych technologii i tradycyjnych metod geodezyjnych sprawiło, że jesteśmy zaufanym i niezawodnym źródłem zapewniającym precyzyjne granice i dane topograficzne, a także dostarczającym niezbędną dokumentację prawną.",
             "viewMoreText": "Czytaj więcej",
             "cardTitle1": "Podział działki i nieruchomości",
-            "cardText1": "Geodezyjny podział działki i nieruchomości to fizyczne wyodrębnienie z pierwotnej działki ewidencyjnej dwóch lub większej ilości działek, zawierających się w granicach działki sprzed podziału.",
+            "cardText1": "Geodezyjny podział działki i nieruchomości to fizyczne wyodrębnienie z pierwotnej działki ewidencyjnej dwóch lub większej ilości działek, zawierających się w granicach działki sprzed podziału. Efektem tego podziału jest nowa konfiguracja działek o nowym układzie linii granicznych i powierzchniach. Dokumentacja powstająca w trakcie wykonywania podziału ma formę operatu technicznego. Dokładne zalecenia na temat ewidencyjnego rozbicia działki są ujęte w miejscowym planie zagospodarowania przestrzennego obowiązującego na terenie danej gminy. Koszt podziału działki jest uzależniony od jej wielkości.",
             "cardTitle2": "Tyczenie budynków",
-            "cardText2": "Geodezyjne wytyczenie budynków w terenie służy przestrzennemu umiejscowieniu tych obiektów zgodnie z projektem budowlanym.",
+            "cardText2": "Geodezyjne wytyczenie budynków w terenie służy przestrzennemu umiejscowieniu tych obiektów zgodnie z projektem budowlanym. Ważne jest zachowanie odpowiednich odległości wyznaczanych obiektów w stosunku do obiektów istniejących i wznoszonych oraz względem granic nieruchomości. Geodezyjne wytyczanie obiektów budowlanych należy do prac przygotowawczych, do których można przystąpić wyłącznie na terenie objętym pozwoleniem na budowę lub zgłoszeniem.",
             "cardTitle3": "Wytyczenie i ustalenie granic nieruchomości",
             "cardText3": "Granica działki określana jest przez znaki i punkty graniczne. Znaki graniczne wskazują położenie linii granicznej, zaś punkty graniczne, oznaczają miejsca, gdzie posesja się kończy.",
             "cardTitle4": "Obsługa geodezyjna inwestycji budowlanych",
             "cardText4": "Obsługa geodezyjna inwestycji budowlanych to wszystkie czynności wykonywane podczas realizacji wszelkich inwestycji budowlanych.",
             "cardTitle5": "Prace geodezyjne na terenach kolejowych",
-            "cardText5": "Prace geodezyjne na obszarach kolejowych obejmują precyzyjne pomiary i oceny torów i infrastruktury w celu utrzymania bezpieczeństwa i wydajności operacyjnej, zapewniając integralność sieci kolejowej.",
+            "cardText5": "Prace geodezyjne na obszarach kolejowych obejmują precyzyjne pomiary i oceny torów, a także infrastruktury w celu utrzymania bezpieczeństwa i wydajności operacyjnej, zapewniając integralność sieci kolejowej.",
             "cardTitle6": "Mapy do celów projektowych",
             "cardText6": "Mapa do celów projektowych to projekt zagospodarowania działki lub terenu sporządzany na kopii aktualnej mapy zasadniczej.",
 
@@ -221,15 +221,15 @@ module.exports = {
                     ]
                 },
                 "4": {
-                    "title": "Obsługa geodezyjna inwestycji budowlanych",
-                    "desc": "Obsługa geodezyjna inwestycji budowlanych to wszystkie czynności wykonywane podczas realizacji wszelkich inwestycji budowlanych.\nUsługa ta obejmuje:\n",
+                    "title": "Surveying services of construction investments",
+                    "desc": "Geodetic services of construction investments include all activities performed during the implementation of all kind of construction investments. \n This service includes:",
                     "list": [
-                        "założenie oraz pomiar osnowy realizacyjnej",
-                        "wyznaczenie granic terenu budowy",
-                        "wytyczenie głównych osi obiektów budowlanych",
-                        "założenie roboczych znaków wysokościowych",
-                        "stworzenie powykonawczej dokumentacji geodezyjno-kartograficznej",
-                        "badanie przemieszczeń i odkształceń"
+                        "establishing control and control survey",
+                        "boundary determination of construction land",
+                        "alignment of buildings axes",
+                        "installation of temporary height marks",
+                        "creation of as-built survey and cartographic documentation",
+                        "testing of displacements and deformations"
                     ]
                 },
                 "5": {},
@@ -296,10 +296,10 @@ module.exports = {
     pricing: {
 
         "en": {
-            "secondTitle": "Mapping your future at fair price",
-            "secondDesc": "Get to know what services we offer for all your land surveying needs.",
-            "pricingDesc": "Our pricing depends on the schedule and size of the project. For more information, please contact us.",
-            "lastTitle": "Check out our contact information",
+            "secondTitle": "Mapping the future at fair price",
+            "secondDesc": "Find out what services we provide.",
+            "pricingDesc": "The cost of each project is estimated individually. It depends on the complexity, time and resources necessary to implement the project. For more information on the costs of cooperation, please contact us by phone or e-mail.",
+            "lastTitle": "Check out our contact details",
             "contactButton": "Contact us"
 
 
@@ -356,7 +356,7 @@ module.exports = {
     contactUs: {
         "en": {
             "mainTitle": "Contact us",
-            "mainDesc": "For any inquiries about your specific project needs, don't hesitate to reach out. I'm glad to assist you.",
+            "mainDesc": "If you have any questions about your project needs, please don't hesitate to contact us. I'll be glad to help you.",
             "emailLabel": "Email",
             "emailText": "dakowiczanna@interia.pl",
             "phoneLabel": "Phone",
@@ -375,7 +375,7 @@ module.exports = {
             "saturdaySchedule": "Closed",
             "sunday": "Sunday: ",
             "sundaySchedule": "Closed",
-            "goodbyeText": "Hope to collaborate with you soon!"
+            "goodbyeText": "I hope we can work together soon!"
 
         },
         "pl": {
@@ -406,7 +406,7 @@ module.exports = {
     aboutUs: {
         "en": {
             "mainTitle": "About us",
-            "aboutUsIntro": "Hello! I'm Anna, a passionate land surveyor with over a decade of experience in this captivating field. As I've grown in my profession, I've discovered an unwavering commitment to precision and a deep love for the intricacies of land surveying. The journey of this company began with a vision: to provide top-tier land surveying services that blend cutting-edge technology with a personal touch. Drawing from my deep-rooted interest in the field, I founded this enterprise to make a positive impact in the world of land measurement.",
+            "aboutUsIntro": "I have been providing surveying services for over thirteen years. Land surveying has always been my professional dream, which I manage to realize efficiently. Over time, I gained the necessary experience and knowledge to start my own business and start providing services at the highest level. In my work, I use modern technologies, which are used to make my clients dreams come true. I invite you to cooperate with me.",
             "secondTitle": "Our contact information"
         },
         "pl": {
